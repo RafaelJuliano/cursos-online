444e53a83acf0fe9c7580ac839d8913ef94f2245
Rafael Juliano Ferreira <67478757+RafaelJuliano@users.noreply.github.com>
2022-04-04 19:34:09 -0300

    solve course index

--- src/resources/views/course/index.blade.php
+++ src/resources/views/course/index.blade.php
@@ -3,18 +3,20 @@
 @section('content')
 <div class="container">
 
-    <div class="d-flex flex-row flex-wrap p-2 justify-content-start">
-    @foreach($courses as $course)
-        <div class="card m-2" style="width: 15vw;">
-            <div class="card-body d-flex flex-column align-items-stretch">
-                <h5 class="card-title">{{ $course->title }}</h5>
-                <h6 class="card-subtitle mb-2 text-muted">Criado por: {{ $course->createdBy->name }}</h6>
-                <p class="card-text">{{ $course->description }}</p>
-                <a href="{{ route('cursos.show', $course->id) }}" class="card-link mt-auto">Detalhes do Curso</a>            
+    
+        <div class="d-flex flex-row flex-wrap justify-content-center">
+        @foreach($courses as $course)
+            <div class="card m-2 col-xl-3 col-lg-3 col-md-5 col-sm-12 col-12" >
+                <div class="card-body d-flex flex-column align-items-stretch">
+                    <h5 class="card-title">{{ $course->title }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Criado por: {{ $course->createdBy->name }}</h6>
+                    <p class="card-text" style="max-height: 200px; overflow: hidden; text-overflow: ellipsis;">{{ $course->description }}</p>
+                    <a href="{{ route('cursos.show', $course->id) }}" class="card-link mt-auto ">Detalhes do Curso</a>            
+                </div>
             </div>
+        @endforeach
         </div>
-    @endforeach
-    </div>
+    
 
 </div>
 @endsection
